02ecfeb2bd7669ba34a62f1946cbab271a1688e9
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-10-30 13:52:52 +0000

    deployment

--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,13 @@ const bookModel = require('./book')
 const authorModel = require('./author')
 const genreModel = require('./genre')
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, NODE_ENV } = process.env
 
 const setupDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
-    dialect: 'mysql',
+    dialect: NODE_ENV === 'production' ? 'postgres' : 'mysql',
     logging: false
   })
 
