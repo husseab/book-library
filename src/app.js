e7f4076d61819eeed29c5a734140f59a8e848504
abdikarim hussein <abdikarim.hussein557@gmail.com>
2022-11-17 05:51:48 +0000

    minor fixes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (_, res) => {
-  res.status(200).json({ result: 'Welcome to Book Library app! Use /books, /authors, /genres and /readers paths to see what is available.' })
+  res.status(200).json({ result: 'Welcome to the Back End DB! Use /books, /authors, /genres and /readers paths to see what is available.' })
 })
 
 app.use('/readers', readerModel)
